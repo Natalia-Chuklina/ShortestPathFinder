3b575a3593c1c4ee65bf43baa20146fe524cd4a4
Natalia-Chuklina <natalia.chuklina.98@gmail.com>
2022-06-05 21:34:53 +0700

    Initial commit.

---
+++ src/main/java/ru/nc/shortestpathfinder/Application.java
@@ -0,0 +1,12 @@
+package ru.nc.shortestpathfinder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class.getName());
+
+    public static void main(String[] args) {
+        log.info("Application start.");
+    }
+}
