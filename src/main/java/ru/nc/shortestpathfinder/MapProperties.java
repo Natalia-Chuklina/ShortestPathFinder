a844cdc59edc63bb3b968246fd6738e60c01f677
Natalia-Chuklina <natalia.chuklina.98@gmail.com>
2022-06-10 22:52:51 +0700

    Добавила класс для хранения свойств карты.

---
+++ src/main/java/ru/nc/shortestpathfinder/MapProperties.java
@@ -0,0 +1,32 @@
+package ru.nc.shortestpathfinder;
+
+import java.util.Map;
+
+public class MapProperties {
+
+    private final Map<String, Integer> raceTiles;
+    private final String mapCells;
+
+    private final int fieldDimension;
+
+    public MapProperties(String race, String mapCells) throws RuntimeException {
+        PropertiesReader reader = new PropertiesReader(race, mapCells);
+        raceTiles = reader.getRaceTiles();
+
+        this.mapCells = mapCells;
+
+        this.fieldDimension = (int) Math.sqrt(this.mapCells.length());
+    }
+
+    private String getTileByIndex(int index) {
+        return this.mapCells.substring(index, index + 1);
+    }
+
+    public int getWeightByIndex(int index) {
+        return this.raceTiles.get(this.getTileByIndex(index));
+    }
+
+    public int getFieldDimension() {
+        return this.fieldDimension;
+    }
+}
