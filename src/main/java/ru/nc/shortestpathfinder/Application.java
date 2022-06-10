2105d3722daa4089b5a9e74669fdcc7066e05bec
Natalia-Chuklina <natalia.chuklina.98@gmail.com>
2022-06-10 22:51:12 +0700

    Добавила вызов метода getResult.

--- src/main/java/ru/nc/shortestpathfinder/Application.java
+++ src/main/java/ru/nc/shortestpathfinder/Application.java
@@ -5,8 +5,13 @@ import org.slf4j.LoggerFactory;
 
 public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class.getName());
+    private static final String field = "SSSS";
 
     public static void main(String[] args) {
         log.info("Application start.");
+
+
+        log.info("The result is {}", Solution.getResult(field, "Human"));
+
     }
 }
